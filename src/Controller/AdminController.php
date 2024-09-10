8a13f87aae5275b2bf157d3812f754dac51a171f
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2024-09-10 13:39:11 +0200

    Add API settings

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -29,14 +29,89 @@ class AdminController extends Singleton
 		);
 	}
 
+	public function action_admin_init() {
+		register_setting( 'syncengine', 'syncengine' );
+
+		add_settings_section(
+			'api',
+			__( 'SyncEngine API' ),
+			array( $this, 'settings_api_section' ),
+			'syncengine'
+		);
+
+		add_settings_field(
+			'host',
+			__( 'Domain/Host' ),
+			array( $this, 'settings_api_field_input' ),
+			'syncengine',
+			'api',
+			[
+				'name'        => 'host',
+				'placeholder' => __( 'https://' ),
+				'section'     => 'api',
+				'setting'     => 'syncengine',
+			]
+		);
+
+		add_settings_field(
+			'token',
+			__( 'Token' ),
+			array( $this, 'settings_api_field_input' ),
+			'syncengine',
+			'api',
+			[
+				'type'        => is_super_admin() ? 'text' : 'password',
+				'name'        => 'token',
+				'placeholder' => __( '#' ),
+				'section'     => 'api',
+				'setting'     => 'syncengine',
+			]
+		);
+
+		add_settings_field(
+			'version',
+			__( 'Version' ),
+			array( $this, 'settings_api_field_input' ),
+			'syncengine',
+			'api',
+			[
+				'type'        => 'number',
+				'name'        => 'version',
+				'placeholder' => __( '1' ),
+				'section'     => 'api',
+				'setting'     => 'syncengine',
+			]
+		);
+	}
+
 	public function page() {
 		?>
 		<div class="wrap">
 			<form action='options.php' method='post'>
 				<h1><?= __( 'SyncEngine', 'syncengine' ) ?></h1>
+				<?php
+				settings_fields( 'syncengine' );
+				do_settings_sections( 'syncengine' );
+				submit_button();
+				?>
 			</form>
 		</div>
 		<?php
 	}
 
+	public function settings_api_section( $args ) {
+		return $args['title'] ?? '';
+	}
+
+	public function settings_api_field_input( $args ) {
+		$type = $args['type'] ?? 'text';
+		$options = get_option( $args['setting'] );
+		$id = $args['setting'] . '_' . $args['section'] . '_' . $args['name'];
+		$name = $args['setting'] . '[' . $args['section'] . ']' . '[' . $args['name'] . ']';
+
+		$value = $options[ $args['section'] ][ $args['name'] ] ?? '';
+		?>
+		<input id="<?= $id ?>" type="<?= $type ?>" name="<?= $name; ?>" value="<?= $value; ?>" placeholder="<?= $args['placeholder'] ?? $args['label'] ?? $args['title'] ?>" />
+		<?php
+	}
 }
