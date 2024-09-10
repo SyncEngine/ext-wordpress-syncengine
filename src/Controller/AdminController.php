b7fad553611bc99401a5781fec3651809bf3dac5
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2024-09-10 13:39:01 +0200

    Start on admin page

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,17 +2,41 @@
 
 namespace SyncEngine\WordPress\Controller;
 
-class AdminController
-{
-	private static $_instance;
+use SyncEngine\WordPress\Service\Singleton;
 
-	public static function get_instance()
-	{
-		if ( ! isset( self::$_instance ) ) {
-			self::$_instance = new self();
-		}
-		return self::$_instance;
+class AdminController extends Singleton
+{
+	const CAPABILITY = 'syncengine';
+
+	public function register() {
+		add_action( 'admin_menu', array( $this, 'action_admin_menu' ) );
+		add_action( 'admin_init', array( $this, 'action_admin_init' ) );
 	}
 
-	protected function __construct() {}
+	public function action_admin_menu() {
+		$cap = self::CAPABILITY;
+		if ( ! is_multisite() && ! current_user_can( $cap ) ) {
+			$cap = 'manage_options';
+		}
+
+		add_submenu_page(
+			'tools.php',
+			__( 'SyncEngine', 'syncengine' ),
+			__( 'SyncEngine', 'syncengine' ),
+			$cap,
+			'syncengine',
+			array( $this, 'page' ),
+		);
+	}
+
+	public function page() {
+		?>
+		<div class="wrap">
+			<form action='options.php' method='post'>
+				<h1><?= __( 'SyncEngine', 'syncengine' ) ?></h1>
+			</form>
+		</div>
+		<?php
+	}
+
 }
