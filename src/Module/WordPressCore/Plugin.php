656576e9200f379acd44be5321b47622c92d51a7
JoryHogeveen <info@keraweb.nl>
2026-04-10 00:02:20 +0200

    Add refresh endpoint to clear caches

--- src/Module/WordPressCore/Plugin.php
+++ src/Module/WordPressCore/Plugin.php
@@ -22,11 +22,16 @@ class Plugin extends Singleton
 		}
 
 		add_action( 'update_option_syncengine', [ $this, 'action_update_option_syncengine' ], 10, 2 );
+		add_action( 'syncengine_cache_refresh', [ $this, 'action_syncengine_cache_refresh' ], 10, 1 );
 
 		$this->registered = true;
 	}
 
 	public function action_update_option_syncengine( $old_value, $value ) {
+		PlatformService::get_instance()->clearTriggerEndpointMapCache();
+	}
+
+	public function action_syncengine_cache_refresh( $context = [] ) {
 		PlatformService::get_instance()->clearTriggerEndpointMapCache();
 	}
 }
--- src/Rest/RestRoute.php
+++ src/Rest/RestRoute.php
@@ -6,12 +6,25 @@ use SyncEngine\WordPress\Service\Singleton;
 
 class RestRoute extends Singleton
 {
+	const REFRESH_THROTTLE_TRANSIENT = 'syncengine_refresh_throttle';
+	const REFRESH_THROTTLE_SECONDS   = 30;
+
 	public function register() {
 		register_rest_route(
 			'syncengine/v1',
 			'status',
 			[
-				'callback' => [ $this, 'statusCallback' ],
+				'callback'            => [ $this, 'statusCallback' ],
+				'permission_callback' => '__return_true',
+			]
+		);
+
+		register_rest_route(
+			'syncengine/v1',
+			'refresh',
+			[
+				'methods'             => 'POST',
+				'callback'            => [ $this, 'refreshCallback' ],
 				'permission_callback' => '__return_true',
 			]
 		);
@@ -20,7 +33,30 @@ class RestRoute extends Singleton
 	public function statusCallback() {
 		return [
 			'success' => true,
-			'status'  => 'active'
+			'status'  => 'active',
+		];
+	}
+
+	public function refreshCallback() {
+		if ( get_transient( self::REFRESH_THROTTLE_TRANSIENT ) ) {
+			return [
+				'success'   => true,
+				'refreshed' => false,
+				'reason'    => 'throttled',
+			];
+		}
+
+		set_transient( self::REFRESH_THROTTLE_TRANSIENT, 1, self::REFRESH_THROTTLE_SECONDS );
+
+		do_action( 'syncengine_cache_refresh', [
+			'source' => 'rest_route',
+			'route'  => 'syncengine/v1/refresh',
+		] );
+
+		return [
+			'success'   => true,
+			'refreshed' => true,
+			'timestamp' => time(),
 		];
 	}
 }
